77a1aa5380601735b92ab8ce25bfbb0a1d4f8bf7
Robert Cowher <bobcowher@gmail.com>
2022-10-30 16:48:27 -0500

    Broken down into more usable functions. Old pong_human file added for reference

--- agent.py
+++ agent.py
@@ -1,6 +1,70 @@
+import threading
+
 from pong import Pong
+import time
+import torch
+import pygame
+import random
+from collections import deque
+from model import Linear_QNet, QTrainer
+import numpy as np
 
-game = Pong()
 
-game.play()
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+NUM_ITERATIONS = 1000000
+LR = 0.001
 
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft() on max memory
+        self.model = Linear_QNet(11, 256, 2)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+    def get_action(self, state):
+        move_options = ["UP", "DOWN"]
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 1)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return move_options[final_move]
+
+    def get_state(self, game):
+        current_screen = pygame.image.tostring(game.screen, 'RGB')
+        return np.array(current_screen)
+
+def train():
+
+    game = Pong(ai_player_count=1)
+    agent = Agent()
+
+    carryOn = True
+    # -------- Main Program Loop -----------
+    while carryOn:
+        # --- Main event loop
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                carryOn = False  # Flag that we are done so we exit this loop
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:  # Pressing the x Key will quit the game
+                    carryOn = False
+
+        game.play_turn(agent.get_action())
+
+    agent.get_state(game)
+
+    game.end_game()
+
+if __name__ == '__main__':
+    train()
--- pong.py
+++ pong.py
@@ -1,125 +1,122 @@
 import pygame
 from paddle import Paddle
 from ball import Ball
+import copy
+import queue
 from queue import Queue
+import random
 
 
 class Pong:
 
 
-    def __init__(self, color1=(0, 0, 0), color2=(255, 255, 255), size=(700, 500), ai_player_count=1):
+    def __init__(self, color1=(0, 0, 0), color2=(255, 255, 255), size=(700, 500), ai_player_count=1, ai_1_queue=None):
         self.color1 = color1
         self.color2 = color2
         self.size = (700, 500)
         self.screen = pygame.display.set_mode(size)
         self.ai_player_count = ai_player_count # TODO: Make this variable useful.
-        self.ai_key_press = []
-
-
-    def get_ai_key_press(self):
-        event = self.ai_key_press
-        self.ai_key_press = None
-        return event
-
-    def set_ai_key_press(self, key):
-        self.ai_key_press
-
-    def play(self):
+        self.ai_move = [0, 0]
+        self.ai_1_queue = ai_1_queue
         pygame.display.set_caption("Pong")
         pygame.init()
-        paddleA = Paddle(self.color2, 10, 100)
-        paddleA.rect.x = 20
-        paddleA.rect.y = 200
+        self.paddleA = Paddle(self.color2, 10, 100)
+        self.paddleA.rect.x = 20
+        self.paddleA.rect.y = 200
 
-        paddleB = Paddle(self.color2, 10, 100)
-        paddleB.rect.x = 670
-        paddleB.rect.y = 200
+        self.paddleB = Paddle(self.color2, 10, 100)
+        self.paddleB.rect.x = 670
+        self.paddleB.rect.y = 200
 
-        ball = Ball(self.color2, 10, 10)
-        ball.rect.x = 345
-        ball.rect.y = 195
+        self.ball = Ball(self.color2, 10, 10)
+        self.ball.rect.x = 345
+        self.ball.rect.y = 195
 
         # This will be a list that will contain all the sprites we intend to use in our game.
-        all_sprites_list = pygame.sprite.Group()
+        self.all_sprites_list = pygame.sprite.Group()
 
         # Add the 2 paddles and the ball to the list of objects
-        all_sprites_list.add(paddleA)
-        all_sprites_list.add(paddleB)
-        all_sprites_list.add(ball)
+        self.all_sprites_list.add(self.paddleA)
+        self.all_sprites_list.add(self.paddleB)
+        self.all_sprites_list.add(self.ball)
 
         # The loop will carry on until the user exits the game (e.g. clicks the close button).
-        carryOn = True
+
 
         # The clock will be used to control how fast the screen updates
-        clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-        # Initialise player scores
-        scoreA = 0
-        scoreB = 0
+        self.scoreA = 0
+        self.scoreB = 0
 
-        # -------- Main Program Loop -----------
-        while carryOn:
-            # --- Main event loop
-            for event in pygame.event.get():  # User did something
-                if event.type == pygame.QUIT:  # If user clicked close
-                    carryOn = False  # Flag that we are done so we exit this loop
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_x:  # Pressing the x Key will quit the game
-                        carryOn = False
+    def end_game(self):
+        pygame.quit()
 
-            # Moving the paddles when the use uses the arrow keys (player A) or "W/S" keys (player B)
-            keys = pygame.key.get_pressed()
+
+
+    def play_turn(self, action):
+        # Moving the paddles when the use uses the arrow keys (player A) or "W/S" keys (player B)
+        keys = pygame.key.get_pressed()
+        if self.ai_player_count == 0:
             if keys[pygame.K_w]:
-                paddleA.moveUp(5)
+                self.paddleA.moveUp(5)
             if keys[pygame.K_s]:
-                paddleA.moveDown(5)
+                self.paddleA.moveDown(5)
             if keys[pygame.K_UP]:
-                paddleB.moveUp(5)
+                self.paddleB.moveUp(5)
             if keys[pygame.K_DOWN]:
-                paddleB.moveDown(5)
+                self.paddleB.moveDown(5)
+        if self.ai_player_count == 1:
+            if action == "UP":
+                self.paddleA.moveUp(5)
+            if action == "DOWN":
+                self.paddleA.moveDown(5)
+            if keys[pygame.K_UP]:
+                self.paddleB.moveUp(5)
+            if keys[pygame.K_DOWN]:
+                self.paddleB.moveDown(5)
 
+            # --- Game logic should go here
+        self.all_sprites_list.update()
 
+        # Check if the ball is bouncing against any of the 4 walls:
+        if self.ball.rect.x >= 690:
+            self.scoreA += 1
+            self.ball.velocity[0] = -self.ball.velocity[0]
+        if self.ball.rect.x <= 0:
+            self.scoreB += 1
+            self.ball.velocity[0] = -self.ball.velocity[0]
+        if self.ball.rect.y > 490:
+            self.ball.velocity[1] = -self.ball.velocity[1]
+        if self.ball.rect.y < 0:
+            self.ball.velocity[1] = -self.ball.velocity[1]
 
-                # --- Game logic should go here
-            all_sprites_list.update()
+            # Detect collisions between the ball and the paddles
+        if pygame.sprite.collide_mask(self.ball, self.paddleA) or pygame.sprite.collide_mask(self.ball, self.paddleB):
+            self.ball.bounce()
 
-            # Check if the ball is bouncing against any of the 4 walls:
-            if ball.rect.x >= 690:
-                scoreA += 1
-                ball.velocity[0] = -ball.velocity[0]
-            if ball.rect.x <= 0:
-                scoreB += 1
-                ball.velocity[0] = -ball.velocity[0]
-            if ball.rect.y > 490:
-                ball.velocity[1] = -ball.velocity[1]
-            if ball.rect.y < 0:
-                ball.velocity[1] = -ball.velocity[1]
+        # --- Drawing code should go here
+        # First, clear the screen to black.
+        self.screen.fill(self.color1)
+        # Draw the net
+        pygame.draw.line(self.screen, self.color2, [349, 0], [349, 500], 5)
 
-                # Detect collisions between the ball and the paddles
-            if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
-                ball.bounce()
+        # Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+        self.all_sprites_list.draw(self.screen)
 
-            # --- Drawing code should go here
-            # First, clear the screen to black.
-            self.screen.fill(self.color1)
-            # Draw the net
-            pygame.draw.line(self.screen, self.color2, [349, 0], [349, 500], 5)
+        # Display scores:
+        font = pygame.font.Font(None, 74)
+        text = font.render(str(self.scoreA), 1, self.color2)
+        self.screen.blit(text, (250, 10))
+        text = font.render(str(self.scoreB), 1, self.color2)
+        self.screen.blit(text, (420, 10))
 
-            # Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
-            all_sprites_list.draw(self.screen)
+        # --- Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
 
-            # Display scores:
-            font = pygame.font.Font(None, 74)
-            text = font.render(str(scoreA), 1, self.color2)
-            self.screen.blit(text, (250, 10))
-            text = font.render(str(scoreB), 1, self.color2)
-            self.screen.blit(text, (420, 10))
+        # --- Limit to 60 frames per second
+        self.clock.tick(60)
 
-            # --- Go ahead and update the screen with what we've drawn.
-            pygame.display.flip()
-
-            # --- Limit to 60 frames per second
-            clock.tick(60)
+        return self.scoreA
 
 # Once we have exited the main program loop we can stop the game engine:
-        pygame.quit()
